f7a20fa1d24b5be64d0994847e4d71ed56ae8609
mirzawets <93333417+mirzawets@users.noreply.github.com>
2023-03-30 18:39:57 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+const btnNext = document.getElementById('btn-next');
+const btnPrev = document.getElementById('btn-prev');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+function activeSlide(i) {
+  slides.forEach((slide) => slide.classList.remove('active'));
+  slides[i].classList.add('active');
+}
+
+function activeDot(i) {
+  dots.forEach((dot) => dot.classList.remove('active'));
+  dots[i].classList.add('active');
+}
+
+let index = 0;
+
+function nextSlide() {
+  if (index === slides.length - 1) {
+    index = 0;
+    activeSlide(index);
+    activeDot(index);
+  } else {
+    index++;
+    activeSlide(index);
+    activeDot(index);
+  }
+}
+
+function prevSlide() {
+  if (index === 0) {
+    index = slides.length - 1;
+    activeSlide(index);
+    activeDot(index);
+  } else {
+    index--;
+    activeSlide(index);
+    activeDot(index);
+  }
+}
+
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide);
+
+dots.forEach((dot, dotIndex) => {
+  dot.addEventListener('click', function () {
+    index = dotIndex;
+    activeSlide(index);
+    activeDot(index);
+  });
+});
+
+const timeInterval = setInterval(nextSlide, 2500);
